411f6e541ea0992d0231ed10ba7cd8794ab5b0e7
weeneet <vinittamboli31@gmail.com>
2024-07-26 13:36:26 +0530

    Add BackendProject's own node_modules and DBConnection code

--- BackendProject/src/index.js
+++ BackendProject/src/index.js
@@ -1,18 +1,24 @@
-import mongoose, { mongo } from "mongoose"
-import { DB_Name } from "./constants"
+// require('dotenv').config({path: './env'})
+console.log("In main index file")
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
 
+dotenv.config({
+    path: './env'
+})
+connectDB();
 
 // import express from "express"
 // const app = express()
-// 
+
 // ;(async () => {
 //     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`)
+//         await mongoose.connect(`${process.env.MONGODB_URI}/VideoTube`)
 //         app.on("error", (error) => {
 //             console.log("Error: ", error);
 //             throw error
 //         })
-// 
+
 //         app.listen(process.env.PORT, () => {
 //             console.log(`App is listening on port ${process.env.PORT}`)
 //         })
