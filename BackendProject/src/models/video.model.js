06a5eb6370cde7efc91703df9ca317389640d815
weeneet <vinittamboli31@gmail.com>
2024-07-27 09:30:13 +0530

    add encryption and token generation

---
+++ BackendProject/src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose, {mongo, Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, //Cloudinary link
+            required: true
+        },
+        thumbnail: {
+            type: String, //Cloudinary link
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number, 
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+const Video = mongoose.model("Video", videoSchema)
